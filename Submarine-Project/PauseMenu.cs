e9896c275a5633db5268923aeeb45977264cb514
Ariana Castro <56000837+ArianaCastro01@users.noreply.github.com>
2023-03-21 14:52:59 -0400

    Update PauseMenu.cs

--- Submarine-Project/PauseMenu.cs
+++ Submarine-Project/PauseMenu.cs
@@ -16,42 +16,18 @@ public class PauseMenu : MonoBehaviour
         {
             if (GameIsPaused == false)
             {
-                GameIsPaused = true;
-                pauseMenuUI.SetActive(true);
+                PauseMenuUI.SetActive(true); //shows the pause menu and its child objects, the panels & buttons
+                Time.timeScale = 0f;  //freezes time in the game world, effectively pausing
+                GameIsPaused = true;  //we use this boolean to track if the game is paused
             }
             
             else
             {
-                GameIsPaused = false;
-                pauseMenuUI.SetActive(false);
-            }
-
-            
-           /* if (GameIsPaused)
-            { Resume(); }
-            else
-            { Pause(); }         //Pause & Resume are not Unity functions, we need to write them */
-            
-        } 
-
-
-    }
-
-    public void Pause()
-    {
-        pauseMenuUI.SetActive(true); //shows the pause menu and its child objects, the panels & buttons
-        Time.timeScale = 0f;  //freezes time in the game world, effectively pausing
-       GameIsPaused = true;  //we use this boolean to track if the game is paused
-
-    }
-
-    public void Resume(bool tf)
-    {
-        pauseMenuUI.SetActive(false); //hides the pause menu
-
-        Time.timeScale = 1f;  //starts time in the game world
-        GameIsPaused = tf;  //we use this boolean to track if the game is paused
-        Debug.Log("Resuming...");
+                PauseMenuUI.SetActive(false); //hides the pause menu
+                Time.timeScale = 1f;  //starts time in the game world
+                GameIsPaused = tf;  //we use this boolean to track if the game is paused
+            }  
+        }
     }
 
     public void LoadMenu(bool tf)
